62c73dc3c78a7e3663eddcd11da9205d5907e749
Cem Demirkartal <CemDemirkartal@users.noreply.github.com>
2018-01-25 16:51:48 +0300

    routes

--- vendor/Application/Config/routes.php
+++ vendor/Application/Config/routes.php
@@ -1,5 +1,6 @@
 <?php
 /** Routes */
 return array(
-
+	"/"			=> array("target" => array("Application\Controller", "index"), "name" => "home"),
+	"error"		=> array("target" => array("Application\Controller", "error"), "name" => "error")
 );
